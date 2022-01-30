469fbb9c9c8915f11780794838f9e87dfcbc791f
V1rlan <98362092+V1rlan@users.noreply.github.com>
2022-01-30 14:14:52 +0100

    Update classify.py

--- classify.py
+++ classify.py
@@ -10,6 +10,12 @@ from picamera import PiCamera
 from datetime import datetime, timedelta
 from time import sleep
 
+from orbit import ISS
+import os
+import csv
+from datetime import datetime
+
+
 # Dato l'interpreter e i labels restituisce il dizionario con i risultati
 def dict_of_scores(interpreter, labels):
         risultati = dict()
@@ -17,6 +23,20 @@ def dict_of_scores(interpreter, labels):
         for ii in range(classify.num_classes(interpreter)):
                 risultati[labels[ii]] = valori[ii]
         return risultati
+
+def path_changer_coast(source_path, final_path):
+    # replace(path sorgente, path finale), spostando rinonima
+    os.replace(source_path, "{}/Costa{}.jpg".format(final_path,cont))
+
+def path_changer_not_coast(source_path):
+    # Rimuovo il file
+    os.remove(source_path)
+
+# Aggiunge riga al file csv
+def csv_addline(n_foto, risultati):
+        location = ISS.coordinates()
+        list_writer = csv.writer(list_file, delimiter=",")
+        list_writer.writerow(["Costa_{}".format(cont), location.longitude,location.latitude, datetime.now(), risultati['costa']])
 
 #Tempo di inizio script
 start_time = datetime.now()
@@ -38,7 +58,13 @@ camera = PiCamera()
 camera.resolution = (2592, 1944)
 camera.framerate = 50
 
+# Setup
+cont = 0        # Contatore foto costa
+list_file = open("csv_file.csv", "w")
+list_writer = csv.writer(list_file, delimiter=",")
+list_writer.writerow(['Num Costa','Longitudine','Latitudine','orario','score costa'])
 now_time = datetime.now()
+
 while (now_time < start_time + timedelta(minutes=2)):
         camera.capture(f"{image_file}")
 
@@ -53,7 +79,16 @@ while (now_time < start_time + timedelta(minutes=2)):
 
         # Output
         risultati = dict_of_scores(interpreter, labels)
-        print('foto nuova')
-        print(risultati)
+
+        if (risultati['costa'] > 0.2):
+                csv_addline(cont, risultati)
+
+                path_changer_coast(f"{image_file}", f"{data_dir}")
+                cont += 1
+        else:
+                os.remove(f"{image_file}")
+
         sleep(5)        
         now_time = datetime.now()
+
+list_file.close()
